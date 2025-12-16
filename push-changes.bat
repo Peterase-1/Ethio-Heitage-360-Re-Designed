1f9ea5701d9d76e0a945498f479e55356c512d5d
Petros Asegid <pasegid@gmail.com>
2025-12-16 13:00:00 +0300

    Update package.json and others

--- push-changes.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Adding changes...
-git add .
-echo Committing changes...
-git commit -m "Merge SuperAdminDashboard: Add educational features with heritage sites"
-echo Pushing to remote...
-git push origin main
-echo Done!
-pause
